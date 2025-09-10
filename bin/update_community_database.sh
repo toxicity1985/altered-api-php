93c867d16852f023de413ddb7b99471d53ad49ad
John Quairia <john@toxicity.be>
2025-09-10 17:15:06 +0200

    update commande

--- bin/update_community_database.sh
+++ bin/update_community_database.sh
@@ -7,11 +7,10 @@ git checkout main && git pull
 cd .. || exit
 
 # 1. Exécuter le script
-php bin/application app:market-place:json
 php bin/application app:unique:json BISE
 php bin/application app:unique:json ALIZE
-php bin/application app:unique:json CORE
-php bin/application app:unique:json COREKS
+#php bin/application app:unique:json CORE
+#php bin/application app:unique:json COREKS
 
 # 2. Vérifier que le script s'est bien terminé
 if [ $? -eq 0 ]; then
