3b1320f55260a2a376b8dd2a5130155376b59d83
John Quairia <john@toxicity.be>
2026-04-02 17:51:18 +0200

    update

--- src/Model/CardSetConstant.php
+++ src/Model/CardSetConstant.php
@@ -11,7 +11,18 @@ class CardSetConstant
         self::BISE,
         self::CYCLONE,
         self::DUSTER,
-        self::EOLE
+        self::EOLE,
+        self::PROMO_GENCON_2023,
+        self::PROMO_2024,
+        self::PROMO_2TCS3,
+        self::WCQ25,
+        self::WCF25,
+        self::WCS25,
+        self::JUDGE,
+        self::MUSUBI,
+        self::DUSTERTOP,
+        self::DUSTERCB,
+        self::WCS26
     ];
 
     public const COREKS = 'COREKS';
@@ -21,4 +32,16 @@ class CardSetConstant
     public const CYCLONE = 'CYCLONE';
     public const DUSTER = 'DUSTER';
     public const EOLE = 'EOLE';
+    public const PROMO_GENCON_2023 = 'PROMO_GENCON_2023';
+    public const PROMO_2024 = 'PROMO_2024';
+    public const PROMO_2TCS3 = 'PROMO_2TCS3';
+    public const WCQ25 = 'WCQ25';
+    public const WCF25 = 'WCF25';
+    public const WCS25 = 'WCS25';
+    public const JUDGE = 'JUDGE';
+    public const MUSUBI = 'MUSUBI';
+    public const DUSTERTOP = 'DUSTERTOP';
+    public const DUSTERCB = 'DUSTERCB';
+    public const WCS26 = 'WCS26';
+
 }
